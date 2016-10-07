c3ed790dc141997da8a1955b96cf78c2ed623ab3
Lior <lior.croll@yale.edu>
2016-10-07 03:00:18 +0000

    final?

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(request, response){
+    response.send('Hello MGT-656!');
+});
+
 app.get('/nickname', function(request, response){
     response.send('comfortable-coyote');
 });
 
-app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
-});
+
 
 app.listen(
     process.env.PORT || 4000,
