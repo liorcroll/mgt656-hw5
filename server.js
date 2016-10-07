4657475f9b02f8b5d8ce029bdd0c4b60256624c5
Lior <lior.croll@yale.edu>
2016-10-07 02:57:14 +0000

    update?

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
 
+app.get('/nickname', function(request, response){
+    response.send('comfortable-coyote');
+});
+
 app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
-app.listen(process.env.PORT || 4000);
+app.listen(
+    process.env.PORT || 4000,
+    process.env.IP || 'localhost'
+    );
